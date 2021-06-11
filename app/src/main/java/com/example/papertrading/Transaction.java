76a91b3e87afc482c54d78f9f1b6daa6a244142b
soham <sohampatkar93@gmail.com>
2021-06-11 23:57:03 +0530

    removed gradle.properties

---
+++ app/src/main/java/com/example/papertrading/Transaction.java
@@ -0,0 +1,4 @@
+package com.example.papertrading;
+
+public class Transaction {
+}
---
+++ app/src/main/java/com/example/papertrading/stock_details.java
@@ -0,0 +1,14 @@
+package com.example.papertrading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stock_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_details);
+    }
+}
